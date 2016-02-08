45f5ee1609b963f1e04aead0cd394294fb66f5b9
radkomitev <radkomitev91@gmail.com>
2016-02-08 16:14:37 +0200

    redirect project

--- IOS Application/CareAndShare/CareAndShare/CareAndShare/CoverPageViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "DLPieChart.h"
-
-@interface CoverPageViewController : UIViewController
-@property (weak, nonatomic) IBOutlet DLPieChart *pieChart;
-
-@end
--- IOS Application/CareAndShare/CareAndShare/CareAndShare/HomePageViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface HomePageViewController : UIViewController
-
-@property (weak, nonatomic) IBOutlet UILabel *labelAboutApp;
-
-- (IBAction)buttonAboutApp:(id)sender;
-
-@end
